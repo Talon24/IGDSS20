f5c83b2c350d257579fe58ab44f56b347bc4bdc7
Talon <talontalon24@gmail.com>
2020-05-01 13:04:03 +0200

    Grass Randoming

---
+++ Assets/GrassRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassRotation : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(GetInstanceID());
+        System.Random random = new System.Random(GetInstanceID());
+        transform.Rotate(0, random.Next(0, 360), 0);
+    }
+}
