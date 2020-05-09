910afeac41100810e7ffe40078c1f6c398bfddb9
Talon <talontalon24@gmail.com>
2020-05-09 14:20:03 +0200

    clamp xy

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -8,6 +8,9 @@ public class MouseManager : MonoBehaviour
     public int max_out = 200;
     public float pan_speed = 20;
     public float zoom_speed = 1000;
+    public Vector2 area_limits_x = new Vector2(-100, 100);
+    public Vector2 area_limits_y = new Vector2(-100, 100);
+    public Vector2 area_limits_z = new Vector2(-100, 100);
     private Vector2 moused;
     public Camera cam;
     // Start is called before the first frame update
@@ -34,14 +37,18 @@ public class MouseManager : MonoBehaviour
         {
             pos.z += pan_speed * Time.deltaTime;
         }
-        if (Input.GetKey("+"))
-        {
-            pos.y += pan_speed * Time.deltaTime;
-        }
-        if (Input.GetKey("-"))
-        {
-            pos.y -= pan_speed * Time.deltaTime;
-        }
+        // if (Input.GetKey("+"))
+        // {
+        //     pos.y += pan_speed * Time.deltaTime;
+        // }
+        // if (Input.GetKey("-"))
+        // {
+        //     pos.y -= pan_speed * Time.deltaTime;
+        // }
+
+        pos.x = Mathf.Clamp(pos.x, area_limits_x[0], area_limits_x[1]);
+        pos.z = Mathf.Clamp(pos.z, area_limits_y[0], area_limits_y[1]);
+        Debug.Log(pos);
         cam.transform.position = pos;
 
         cam.transform.Translate(Input.mouseScrollDelta.y * Vector3.forward * zoom_speed * Time.deltaTime);
