d5e6e58dc14531d06e2e85f64741dde3f45b6f8d
Talon <talontalon24@gmail.com>
2020-05-10 14:03:46 +0200

    suboptimal pan

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-    public int max_in = 0;
-    public int max_out = 200;
     public float pan_speed = 20;
     public float zoom_speed = 1000;
     public Vector2 area_limits_x = new Vector2(-100, 100);
     public Vector2 area_limits_y = new Vector2(-100, 100);
-    public Vector2 area_limits_z = new Vector2(-100, 100);
+    public int max_zoom_in = 10;
     public int max_zoom_out = 100;
-    private Vector2 moused;
     public Camera cam;
     // Start is called before the first frame update
     void Start()
@@ -52,10 +49,23 @@ public class MouseManager : MonoBehaviour
         cam.transform.position = pos;
 
         float wheel_delta = Input.mouseScrollDelta.y;
-        if ((wheel_delta > 0 && pos.y <= 10) || (wheel_delta < 0 && pos.y >= max_zoom_out)) {
+        float factor = zoom_speed * Time.deltaTime;
+        if ((wheel_delta > 0 && pos.y - factor<= max_zoom_in) || (wheel_delta < 0 && pos.y + factor >= max_zoom_out)) {
             wheel_delta = 0;
         }
         cam.transform.Translate(wheel_delta * Vector3.forward * zoom_speed * Time.deltaTime);
+
+
+
+        if (Input.GetMouseButton(0)) {
+            float old_y = cam.transform.position.y;
+            float pan_factor = Mathf.Max(((1.0f / (max_zoom_out - old_y) / max_zoom_out) * Time.deltaTime) * 500000.0f, 0.0f);
+            Debug.Log(pan_factor);
+            cam.transform.Translate(new Vector3(-Input.GetAxis("Mouse X") * pan_factor, -Input.GetAxis("Mouse Y") * pan_factor, 0));
+            cam.transform.position = new Vector3(cam.transform.position.x, old_y, cam.transform.position.z);
+        }
+
+
 
     }
 }
