c5ad9b0a4550e8ac65c02e7b2c0df4529e439c51
Talon <talontalon24@gmail.com>
2020-05-10 14:20:07 +0200

    Leftclick gives info

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -11,6 +11,7 @@ public class MouseManager : MonoBehaviour
     public int max_zoom_in = 10;
     public int max_zoom_out = 100;
     public Camera cam;
+    public int tile_layer_mask = 8;
     // Start is called before the first frame update
     void Start()
     {
@@ -57,12 +58,24 @@ public class MouseManager : MonoBehaviour
 
 
 
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetMouseButton(1)) {
             float old_y = cam.transform.position.y;
             float pan_factor = Mathf.Max(((1.0f / (max_zoom_out - old_y) / max_zoom_out) * Time.deltaTime) * 500000.0f, 0.0f);
-            Debug.Log(pan_factor);
+            // Debug.Log(pan_factor);
             cam.transform.Translate(new Vector3(-Input.GetAxis("Mouse X") * pan_factor, -Input.GetAxis("Mouse Y") * pan_factor, 0));
             cam.transform.position = new Vector3(cam.transform.position.x, old_y, cam.transform.position.z);
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 200.0f, 1 << tile_layer_mask)) {
+                if (hit.transform != null){
+                    Debug.Log(string.Format("Clicked tile is {0}", hit.transform.gameObject.name));
+                }
+            }
+
         }
 
 
